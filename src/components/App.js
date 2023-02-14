06d753d8848cb32e1aede20016fef9232bc8036e
Catfish7887 <danya.kleutin@yandex.ru>
2023-02-15 00:54:38 +0300

    profile(in dev)

--- src/components/App.js
+++ src/components/App.js
@@ -12,8 +12,8 @@ function App() {
   return (
     <>
       <Routes>
-        <Route path="/profile" element={<Profile />} />
         <Route path="/" element={<Main />} />
+        <Route path="/profile" element={<ProtectedRoute isLoggedIn={true} component={<Profile />} />} />
         <Route path="/movies" element={<ProtectedRoute isLoggedIn={true} component={<Movies />} />} />
         <Route path="/saved-movies" element={<ProtectedRoute isLoggedIn={true} component={<SavedMovies />} />} />
       </Routes>
--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -5,7 +5,21 @@ function Profile() {
     <>
       <Header />
       <main className="profile">
-        <h1 className="profile__title">111</h1>
+        <h1 className="profile__title">Привет, пользователь!</h1>
+        <ul className="profile__user-info">
+          <li className="profile__info-container">
+            <span className="profile__info-element">Имя</span>
+            <span className="profile__info-element">Пользователь</span>
+          </li>
+          <li className="profile__info-container">
+            <span className="profile__info-element">Email</span>
+            <span className="profile__info-element">email@example.com</span>
+          </li>
+        </ul>
+        <div className="profile__buttons">
+          <button className='profile__button' type="button">Редактировать</button>
+          <button className='profile__button profile__button_red-text' type="button">Выйти из аккаунта</button>
+        </div>
       </main>
     </>
   );

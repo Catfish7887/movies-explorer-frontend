15be3d41c6a49e592ae5efc42ff0f366110ed97a
Catfish7887 <danya.kleutin@yandex.ru>
2023-02-24 05:34:19 +0300

    Финальные штрихи, отправка на ревью

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,16 +1,20 @@
 function Footer() {
-  return(
+  return (
     <footer className="footer">
       <h2 className="footer__title">Учебный проект Яндекс.Практикум х BeatFilm.</h2>
       <div className="footer__container">
         <nav className="footer__links">
-          <a href="https://practicum.yandex.ru/" className="footer__link">Яндекс.Практикум</a>
-          <a href="https://github.com/catfish7887" className="footer__link">Github</a>
+          <a href="https://practicum.yandex.ru/" target={'blank'} className="footer__link">
+            Яндекс.Практикум
+          </a>
+          <a href="https://github.com/catfish7887" target={'blank'} className="footer__link">
+            Github
+          </a>
         </nav>
         <small className="footer__copyright">&copy;Daniil Klochkov, 2022</small>
       </div>
     </footer>
-  )
-};
+  );
+}
 
 export default Footer;
--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -3,13 +3,13 @@ function Portfolio() {
     <section className="portfolio">
       <h2 className="portfolio__title">Портфолио</h2>
       <nav className="portfolio__links">
-        <a className="portfolio__link" href="2">
+        <a className="portfolio__link" href="https://github.com/Catfish7887/how-to-learn" target={'blank'}>
           Статичный сайт<span>↗</span>
         </a>
-        <a className="portfolio__link" href="2">
+        <a className="portfolio__link" href="https://github.com/Catfish7887/russian-travel" target={"blank"}>
           Адаптивный сайт<span>↗</span>
         </a>
-        <a className="portfolio__link" href="2">
+        <a className="portfolio__link" href="https://github.com/Catfish7887/react-mesto-api-full" target={"blank"}>
           Одностраничное приложение<span>↗</span>
         </a>
       </nav>

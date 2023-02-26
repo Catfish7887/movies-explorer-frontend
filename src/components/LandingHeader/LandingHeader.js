941d79d86458ea148f894de729064d51aa7961b8
Catfish7887 <danya.kleutin@yandex.ru>
2023-02-26 21:08:30 +0300

    Pixel perfect(1280px)

--- src/components/LandingHeader/LandingHeader.js
+++ src/components/LandingHeader/LandingHeader.js
@@ -5,7 +5,7 @@ function LandingHeader() {
     <section className="landing-header">
       <div className="landing-header__content">
         <img className="landing-header__logo" src={headerLogo} alt="Логотип" />
-        <h1 className="landing-header__heading">Учебный проект студента факультета Веб&#8209;разработки.</h1>
+        <h1 className="landing-header__heading">Учебный проект студента&nbsp;факультета Веб&#8209;разработки.</h1>
         <span className="landing-header__span">Листайте ниже, чтобы узнать больше про этот проект и&nbsp;его создателя.</span>
         <a href="#about-me" className="landing-header__button">
           Узнать больше
